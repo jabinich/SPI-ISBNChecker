6c54ade8bd8ec467d6eced8b87fcf0210736185a
jabinich <jabinich@gmail.com>
2023-05-21 19:20:42 +0200

    A class ISBNChecker created
    now only ISBN-10 is checked

--- Program.cs
+++ Program.cs
@@ -7,24 +7,17 @@ namespace SPI_ISBNChecker
         static void Main(string[] args)
         {
             bool continueValidation = true;
-            
+
             while (continueValidation)
             {
                 Console.WriteLine("Bitte geben Sie eine 10-stellige ISBN ein:");
 
                 string input = Console.ReadLine() ?? "".Trim();
 
-                //Replace multiple consecutive spaces in a string with a single space
-                input = Regex.Replace(input, @"\s+", " ");
-                
-                //Use regular expressions to check the correctness of isbn-10 format,
-                //e.g. 7-309-04547-5 or 7-309-04547-X or 7 309 04547 5 or 7309045475 
-                bool isbnFormatValidation = (Regex.IsMatch(input, @"^\d+-\d+-\d+-[0-9xX]$") ||
-                                       Regex.IsMatch(input, @"^\d+\s\d+\s\d+\s[0-9xX]$") ||
-                                       Regex.IsMatch(input, @"^[0-9]{9}[0-9xX]$")) &&
-                                       Regex.Replace(input, @"[\s-]", "").Length == 10;
+                ISBNChecker checker = new ISBNChecker();
+                bool isValid = checker.CheckISBN(input);
 
-                if (IsValidISBN(Regex.Replace(input, @"[\s-]", "")) && isbnFormatValidation)
+                if (isValid)
                 {
                     Console.WriteLine("Die eingegebene ISBN ist gueltig.");
                 }
@@ -48,7 +41,7 @@ namespace SPI_ISBNChecker
 
         static bool IsValidISBN(string input)
         {
-           int checksum = 0;
+            int checksum = 0;
             for (int i = 0; i < 9; i++)
             {
                 checksum += (input[i] - '0') * (10 - i);
@@ -61,5 +54,61 @@ namespace SPI_ISBNChecker
             return checksum % 11 == 0;
         }
 
+    }
+
+    public class ISBNChecker {
+
+        private readonly Regex isbnFormatRegex;
+
+        public ISBNChecker()
+        {
+            //Use regular expressions to check the correctness of isbn-10 format,
+            //e.g. 7-309-04547-5 or 7-309-04547-X or 7 309 04547 5 or 7309045475 
+            isbnFormatRegex = new Regex(@"^\d+-\d+-\d+-[0-9xX]$ || 
+                                        ^\d+\s\d+\s\d+\s[0-9xX]$ || 
+                                        ^[0-9]{9}[0-9xX]$");
+        }
+
+        private string CleanInput(string input)
+        {
+            //Replace multiple consecutive spaces in a string with a single space
+            string cleanedInput = Regex.Replace(input, @"\s+", " ");
+            return cleanedInput;
+        }
+
+        public bool CheckISBN(string input)
+        {
+            string cleanedInput = CleanInput(input);
+            bool isFormatValid = CheckFormatISBN(cleanedInput);
+
+            return isFormatValid && CheckValidityISBN(cleanedInput);
+        }
+
+        public bool CheckFormatISBN(string input)
+        {
+            string cleanedInput = CleanInput(input);
+            bool isFormatValid = isbnFormatRegex.IsMatch(cleanedInput) &&
+                                    Regex.Replace(cleanedInput, @"[\s-]", "").Length == 10;
+
+            bool a = isbnFormatRegex.IsMatch(cleanedInput);
+            bool b = Regex.Replace(cleanedInput, @"[\s-]", "").Length == 10;
+
+            return isFormatValid;
+        }
+
+        private bool CheckValidityISBN(string input)
+        {
+            int checksum = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                checksum += (input[i] - '0') * (10 - i);
+            }
+            //check if the last character is number.
+            //If not number (it's X) then convert it to 10 
+            int lastDigit = !Char.IsDigit(input[9]) ? 10 : input[9] - '0';
+            checksum += lastDigit;
+
+            return checksum % 11 == 0;
+        }
     }
 }
